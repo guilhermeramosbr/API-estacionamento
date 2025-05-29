91de961a69a50b26b396aea9b5c53f5d7bd2d283
guilhermeramosbr <ramosguilherme175@gmail.com>
2025-05-28 21:25:47 -0300

    att 0.4

---
+++ Models/Usuario.js
@@ -0,0 +1,25 @@
+import { DataTypes } from 'sequelize';
+import datatabase from '../database.js';
+
+const Usuario = datatabase.define('usuarios', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
